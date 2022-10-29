2dfdc2b690b95664999a9c2e092fbd0a231aa14f
Yuri <yserka@hotmail.com>
2022-10-29 19:14:07 -0300

    add: users image

--- lib/modules/home/widgets/osm_map.dart
+++ lib/modules/home/widgets/osm_map.dart
@@ -6,7 +6,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:logger/logger.dart';
+import 'package:unb/common/cubits/auth/auth_cubit.dart';
 import 'package:unb/common/cubits/group/group_cubit.dart';
 import 'package:unb/common/services/geolocation_service.dart';
 import 'package:unb/common/widgets/loading_indicator.dart';
@@ -21,8 +21,8 @@ class OsmMap extends StatefulWidget {
 class _OsmMapState extends State<OsmMap> {
   final _mapController = MapController();
   final _geoService = Modular.get<GeolocationService>();
-  final _logger = Modular.get<Logger>();
   final _groupCubit = Modular.get<GroupCubit>();
+  final _authCubit = Modular.get<AuthCubit>();
 
   final _centerCurrentLocationStreamController = StreamController<double?>();
   var _centerOnLocationUpdate = CenterOnLocationUpdate.always;
@@ -53,6 +53,27 @@ class _OsmMapState extends State<OsmMap> {
   }
 
   _buildMap(final GroupLoaded state) {
+    final group = state.group.where(
+      (member) => member != (_authCubit.state as AuthLoaded).user,
+    );
+    final markers = group
+        .map(
+          (member) => Marker(
+            point: LatLng(
+              double.parse(member.address!.geo.lat),
+              double.parse(member.address!.geo.lng),
+            ),
+            height: 40,
+            width: 40,
+            builder: (ctx) => CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://picsum.photos/id/${int.parse(member.id) * 100}/200',
+              ),
+            ),
+          ),
+        )
+        .toList();
+
     return FlutterMap(
       mapController: _mapController,
       options: MapOptions(
@@ -67,7 +88,6 @@ class _OsmMapState extends State<OsmMap> {
           }
         },
         onMapReady: () async {
-          _logger.i('[OSM] map ready');
           final pos = await _geoService.determinePosition();
           _mapController.move(LatLng(pos.latitude, pos.longitude), 19);
           _geoService.init();
@@ -107,17 +127,7 @@ class _OsmMapState extends State<OsmMap> {
               _centerCurrentLocationStreamController.stream,
         ),
         MarkerLayer(
-          markers: state.group
-              .map(
-                (member) => Marker(
-                  point: LatLng(
-                    double.parse(member.address!.geo.lat),
-                    double.parse(member.address!.geo.lng),
-                  ),
-                  builder: (ctx) => const Icon(Icons.location_history),
-                ),
-              )
-              .toList(),
+          markers: markers,
         ),
       ],
     );
